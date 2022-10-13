de26244839e02cf8ae792ec669da024e178e1d7a
Teejay Dixon <tahjgdixon@gmail.com>
2022-10-13 02:20:18 -0700

    commit tree a2742db401800b2376f2dedcc7f31bdc674ed7cb

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,27 @@
+class StudentsController < ApplicationController
+
+  def index
+
+    students = Student.all
+    render json: students
+
+  end
+
+
+  def grades
+
+    students = Student.all.order(grade: :desc)
+    render json: students
+
+  end
+
+
+  def highest_grade
+
+    student = Student.all.order(grade: :desc).limit(1)
+    render json: student
+
+  end
+
+
+end
